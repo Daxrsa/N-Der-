854de54e4076e737f398c1d15581d5b299d02186
drenibra <drenibrahimi02@gmail.com>
2022-05-01 17:20:37 +0200

    shperndares table

--- Database/Main.sql
+++ Database/Main.sql
@@ -7,6 +7,19 @@ create table Klienti (
 	PhoneNumber varchar(15),
 	StreetName varchar (40),
 	ZipCode varchar(12) not null,
+	City varchar (30) not null,
+	Role varchar(10)
+)
+
+create table Shperndares (
+	ShperndaresId int identity(1,1) primary key not null,
+	Name varchar(20),
+	Surname varchar(20),
+	Email varchar(60),
+	Password varchar(30),
+	PhoneNumber varchar(15),
+	StreetName varchar (40),
+	ZipCode varchar(12) not null,
 	City varchar (30) not null
 )
 
@@ -31,7 +44,8 @@ alter table Food
 add Restaurant int foreign key references Restaurant(RestaurantId)
 
 create table MyCart(
-	CartId int identity(1,1) not null primary key,
 	KlientiId int not null foreign key references Klienti(KlientiId),
 	RestaurantId int not null foreign key references Restaurant(RestaurantId)
 )
+
+insert into Klienti values ('Dren', 'Ibrahimi', 'di53843@ubt-uni.net', 'test', '+38349724563', 'Rr. Street', '60000', 'Gjilan', 'Admin');
