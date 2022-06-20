2f79ac43f7ec24e6c6f8d3984f5a4654de7f44fb
Daorsa Hyseni <79546481+Daxrsa@users.noreply.github.com>
2022-06-20 23:48:40 +0200

    Update Main.sql

--- Database/Main.sql
+++ Database/Main.sql
@@ -1,61 +1,64 @@
+create database NDere
+use NDere
+
 create table Klienti (
-	Id int identity(1,1) primary key not null,
-	Name varchar(20) not null,
-	Surname varchar(20) not null,
-	Email varchar(60) not null,
-	Password varchar(30) not null,
-	PhoneNumber varchar(15) not null,
-	StreetName varchar (40) not null,
-	ZipCode varchar(12) not null,
-	City varchar (30) not null,
-	Role varchar(10) not null
+	KlientiId int primary key Identity(1,1) not null,
+	Name varchar(max) not null,
+	Surname varchar(max) not null,
+	Email varchar(max) not null,
+	PasswordHash varbinary(max) not null,
+	PasswordSalt varbinary(max) not null,
+	PhoneNumber varchar(max) not null,
+	StreetName varchar (max) not null,
+	ZipCode varchar(max) not null,
+	City varchar (max) not null,
+	Role varchar(max)
 )
 
 create table Shperndares (
-	Id int identity(1,1) primary key not null,
-	Name varchar(20) not null,
-	Surname varchar(20) not null,
-	Email varchar(60) not null,
-	Password varchar(30) not null,
-	PhoneNumber varchar(15) not null,
-	StreetName varchar (40) not null,
-	ZipCode varchar(12) not null,
-	City varchar (30) not null,
-	DriverLicense varbinary(max) NOT NULL
+	ShperndaresId int primary key Identity(1,1) not null,
+	Name varchar(max) not null,
+	Surname varchar(max) not null,
+	Email varchar(max) not null,
+	PasswordHash varbinary(max) not null,
+	PasswordSalt varbinary(max) not null,
+	PhoneNumber varchar(max) not null,
+	StreetName varchar (max) not null,
+	ZipCode varchar(max) not null,
+	City varchar (max) not null,
+	NrPersonal varchar(max) not null,
+	Licensa varbinary(max) 
 )
 
 create table Restaurant(
-	Id int identity(1,1) primary key not null,
-	Name varchar(30) not null,
-	Email varchar(60) not null,
-	Password varchar(30) not null,
-	Address varchar(80) not null,
-	PhoneNumber varchar(15) not null,
-	License varbinary(max) not null
+	RestaurantId int primary key Identity(1,1) not null,
+	Name varchar(max) not null,
+	Email varchar(max) not null,
+	PasswordHash varbinary(max) not null,
+	PasswordSalt varbinary(max) not null,
+	Address varchar(max) not null,
+	PhoneNumber varchar(max) not null,
+	RestaurantImage varbinary(max) 
 )
 
-create table Product (
-	Id int identity(1,1) primary key not null,
-	Name varchar(30) not null,
-	Ingredients varchar(255) not null,
-	Price bigint not null,
-	Currency varchar(50) not null
-	CuisineType varchar(30) not null,
-	Picture varbinary(max) not null
+create table Food (
+	FoodId int primary key Identity(1,1) not null,
+	Name varchar(max) not null,
+	Ingredients varchar(max) not null,
+	Price decimal(5, 2) not null,
+	CuisineType varchar(max) not null,
+	FoodImage varbinary(max) 
 )
+
 alter table Food
-add Restaurant int foreign key references Restaurant(Id) not null
+add Restaurant int foreign key references Restaurant(RestaurantId) not null
 
 create table MyCart(
-	Id int primary key not null identity,
-	KlientiID int not null foreign key references Klienti(Id),
-	RestaurantID int not null foreign key references Restaurant(Id)
+	CartItemId int primary key Identity(1,1) not null,
+	KlientiId int not null foreign key references Klienti(KlientiId),
+	FoodId int not null foreign key references Food(FoodId)
 )
-
-
-insert into Klienti values ('Dren', 'Ibrahimi', 'di53843@ubt-uni.net', 'test', '+38349724563', 'Rr. Street', '60000', 'Gjilan', 'Admin');
-insert into Klienti values('Eros', 'Mehmeti', 'em52473@ubt-uni.net', 'test', '+383xxxxxx', 'Test2', '60000', 'Gjilan', 'Admin');
-insert into Klienti values('Daorsa', 'Hyseni', 'dh51231@ubt-uni.net', 'Testt', '+383xxxxxxxx', 'Test Test Test', '60000', 'Gjilan', 'Admin');
 
 delete from Klienti
 DBCC CHECKIDENT ('[Klienti]', RESEED, 0);
+
